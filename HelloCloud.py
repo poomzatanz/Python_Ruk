a7a893a30dee4117dccfcfbdbcaad22654684753
poomzatan <50354939+poomzatanz@users.noreply.github.com>
2021-04-07 15:20:02 +0700

    Update HelloCloud.py

--- HelloCloud.py
+++ HelloCloud.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-  return "Hello Raksit kongwong from Server"
+  return "Hello 5906021622033 Raksit kongwong from Server"
 
 if __name__ == "__main__":
   server.run(host='0.0.0.0',port=80)
